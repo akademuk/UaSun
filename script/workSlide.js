099679e8098413ab6ab266783ed6a95533aadaf6
akademuk <akademuk24@gmail.com>
2023-05-24 18:09:32 +0300

    sss

--- script/workSlide.js
+++ script/workSlide.js
@@ -39,17 +39,52 @@ $(document).ready(function () {
                 }
             }
         ]
-        
+    });
+
+    function playCenterVideo() {
+        var centerSlide = slider.find(".slick-current");
+        var centerVideo = centerSlide.find("video");
+
+        // Остановка всех видео перед воспроизведением
+        slider.find("video").each(function () {
+            this.pause();
+            this.currentTime = 0;
+        });
+
+        // Воспроизведение видео в центральном слайде
+        if (centerVideo.length > 0) {
+            centerVideo.get(0).play();
+        }
+    }
+
+    slider.on('afterChange', function (event, slick, currentSlide) {
+        playCenterVideo();
+    });
+
+    // Запуск видео при загрузке страницы и при изменении размера окна
+    playCenterVideo();
+
+    $(window).on('resize', function () {
+        var windowWidth = $(window).width();
+
+        if (windowWidth < 1200) {
+            playCenterVideo();
+        } else {
+            // При размере окна больше или равном 1200 пикселей останавливаем видео
+            slider.find("video").each(function () {
+                this.pause();
+                this.currentTime = 0;
+            });
+        }
     });
 
     var slideCount = slider.find('.slick-slide:not(.slick-cloned)').length;
-    var counterText = '<span class="slideIndex">1</span> <span class="slideSeparator">|</span> <span class="slideTotal slideCount">' + slideCount + '</span>'; // Разделение значения на блоки с классами "slideIndex", "slideSeparator" и "slideTotal slideCount"
+    var counterText = '<span class="slideIndex">1</span> <span class="slideSeparator">|</span> <span class="slideTotal slideCount">' + slideCount + '</span>';
     counter.html(counterText);
 
     slider.on('afterChange', function (event, slick, currentSlide) {
         var currentSlideIndex = (currentSlide ? currentSlide : 0) + 1;
-        var updatedCounterText = '<span class="slideIndex">' + currentSlideIndex + '</span> <span class="slideSeparator">|</span> <span class="slideTotal slideCount">' + slideCount + '</span>'; // Обновленное значение с учетом текущего индекса слайда
+        var updatedCounterText = '<span class="slideIndex">' + currentSlideIndex + '</span> <span class="slideSeparator">|</span> <span class="slideTotal slideCount">' + slideCount + '</span>';
         counter.html(updatedCounterText);
     });
-
 });
